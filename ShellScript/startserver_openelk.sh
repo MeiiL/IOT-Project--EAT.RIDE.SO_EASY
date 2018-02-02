43a4607ef4a5e8b41a6117cef91a8bac4eae9e7c
Meii Lyou (Selina) <33171940+MeiiL@users.noreply.github.com>
2018-02-02 03:28:45 +0000

    Update startserver_openelk.sh

--- ShellScript/startserver_openelk.sh
+++ ShellScript/startserver_openelk.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo sysctl -w vm.max_map_count=262144
-sudo docker start 48
-sudo docker exec -it 48 /bin/bash 
+sudo docker start kafka-elk
+sudo docker exec -it kafka-elk /bin/bash 
 if [ $? -eq 0];then
   sh startall_elk_kafka.sh
 else
